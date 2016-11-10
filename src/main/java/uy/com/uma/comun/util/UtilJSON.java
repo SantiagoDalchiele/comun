a7fbbe2a670c23962d1b3ea27e0eda5a6ef8ac4e
Santiago Dalchiele <stgodal@dgi.gub.uy>
2016-11-10 14:53:49 -0300

    Manejo de json

--- src/main/java/uy/com/uma/comun/util/UtilJSON.java
+++ src/main/java/uy/com/uma/comun/util/UtilJSON.java
@@ -1,5 +1,17 @@
 package uy.com.uma.comun.util;
 
+import java.io.StringReader;
+import java.util.Map;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonReader;
+
+
+
 /**
  * Clase de utilería para el manejo de JSON
  *
@@ -7,49 +19,93 @@ package uy.com.uma.comun.util;
  */
 public abstract class UtilJSON {
 	
-	
-	/**
-	 * Retorna el identificador de la propiedad entre comillas dobles seguido de dos puntos (:)
-	 */
-	public static String getPropJSON (String prop) {
-		return "\"" + prop + "\":";
-	}
-	
-	
 
 	/**
-	 * Retorna un valor entre comillas dobles (") y a continuación una coma (,)
+	 * Retorna un objeto JSON con una única propiedad y su correspondiente valor
 	 */
-	public static String getValorJSON (String valor) {
-		return "\"" + valor + "\",";
+	public static JsonObject getJSONObject (String prop, String valor) {
+		return Json.createObjectBuilder().add(prop, valor).build();
 	}
 	
 	
 	
 	/**
-	 * Retorna un valor entre comillas dobles (")
+	 * Retorna un objeto JSON con varias propiedades y sus respectivos valores en la estructura de propiedades
 	 */
-	public static String getComillasJSON (String valor) {
-		return "\"" + valor + "\"";
+	public static JsonObject getJSONObject (Map<String, Object> propiedades) {
+		JsonObjectBuilder job = Json.createObjectBuilder();
+		
+		for (String key : propiedades.keySet()) {
+			Object obj = propiedades.get(key);
+			
+			if (obj instanceof String)
+				job.add(key, (String) obj);
+			else if (obj instanceof Short)
+				job.add(key, (Short) obj);
+			else if (obj instanceof Integer)
+				job.add(key, (Integer) obj);
+			else if (obj instanceof JsonObject)
+				job.add(key, (JsonObject) obj);
+			else if (obj instanceof Object[])
+				job.add(key, getJSONArray((Object []) obj));
+		}
+		
+		return job.build();		
 	}
 	
 	
 	
 	/**
-	 * Retorna el identificador de la propiedad entre comillas dobles seguido de dos puntos (:)
-	 * y valor entre comillas dobles (")
+	 * Retorna un objeto JSON con una única propiedad, el arreglo de objetos
 	 */
-	public static String toJSON (String prop, String valor) {
-		return "\"" + prop + "\":\"" + valor + "\"";
+	public static JsonObject getJSONObject (String prop, Object [] values) {
+		JsonObjectBuilder job = Json.createObjectBuilder();		
+		job.add(prop, getJSONArray(values));
+		return job.build();
 	}
 	
 	
 	
 	/**
-	 * Retorna el identificador de la propiedad entre comillas dobles seguido de dos puntos (:)
-	 * y valor entre comillas dobles (") y a continuación una coma (,)
+	 * Retorna un JsonArray a partir de un arreglo de objetos
 	 */
-	public static String toJSONplus (String prop, String valor) {
-		return toJSON(prop, valor) + ",";
+	public static JsonArray getJSONArray (Object [] values) {
+		JsonArrayBuilder jab = Json.createArrayBuilder();
+		
+		for (Object val : values) {
+			if (val instanceof String)
+				jab.add((String) val);
+			else if (val instanceof Short)
+				jab.add((Short) val);
+			else if (val instanceof Integer)
+				jab.add((Integer) val);
+			else if (val instanceof JsonObject)
+				jab.add((JsonObject) val);
+		}
+		
+		return jab.build();
+	}
+	
+	
+	
+	/**
+	 * Dado los objetos JSON en un string de la forma {...},{...}, ...,{...} retorna los JsonObject en un arreglo
+	 * de objetos
+	 */
+	public static Object [] getJSONObjects (String valores) {
+		if (UtilFormato.esNulo(valores))
+			return new Object[0];
+		else {
+			JsonReader reader = null;
+			
+			try {				
+				reader = Json.createReader(new StringReader("{\"valores\":[" + valores + "]}"));
+				return reader.readObject().getJsonArray("valores").toArray();
+			} catch (Exception e) {
+				return new Object[0];
+			} finally {
+				try { if (reader != null) reader.close(); } catch (Exception e) {}				
+			}
+		}		
 	}
 }
